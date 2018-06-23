92663e299b8ef0a077860922afecb05e649c14f8
nichalrajiv <nichalrajiv@gmail.com>
2018-06-24 03:02:06 +0530

    New class committed

---
+++ FirstGradleApp/library/src/main/java/LibraryImpl.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author madhuri
+ *
+ */
+public class LibraryImpl {
+
+	/**
+	 * 
+	 */
+	public LibraryImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
